55c8586975b39217202c4972b60a315c2905f72d
Kevin Volkov <kevin.volkov.925@my.csun.edu>
2024-11-11 21:42:33 -0800

    'ecommercedb' should be lower case

--- ecom_start/config/db.js
+++ ecom_start/config/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('ecommerceDB', 'root', process.env.MYSQL_ROOT_PASS, { //kevin's comment change 'meta' to your MySQL password
+const sequelize = new Sequelize('ecommercedb', 'root', process.env.MYSQL_ROOT_PASS, { //kevin's comment change 'meta' to your MySQL password
   host: 'localhost',
   dialect: 'mysql',
   //logging: console.log,
