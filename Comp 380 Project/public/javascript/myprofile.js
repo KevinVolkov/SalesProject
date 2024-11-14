23c1f14c8ec6d0809bd524c89e3450747cdf7ccf
Jason Clone <Zanack74@gmail.com>
2024-11-13 20:55:17 -0800

    Delete Comp 380 Project directory
    Signed-off-by: Jason Clone <Zanack74@gmail.com>

--- Comp 380 Project/public/javascript/myprofile.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-
-  // user profile features (editing user profile)
-
-  // admin profile features (editing users in database)
-  
-});
--- Comp 380 Project/public/javascript/signin-button.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const signinButton = document.getElementById("signin-button"); // Login button
-  // Go to register/login screen (auth.ejs) when the login/register button is clicked
-  signinButton.addEventListener("click", () => {
-    window.location.href = "/auth"; // Redirect to the auth page
-  });
-});
